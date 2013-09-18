bd8349b59a94307c6be8d155fb36e356b67be6e0
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-09-18 09:21:04 -0500

    updating display-switcher for my new laptop settings

--- i3/display-chooser.sh
+++ i3/display-chooser.sh
@@ -3,8 +3,8 @@
 xrandr --output VGA-0 --off --output DVI-0 --off --output LVDS --auto
 
 i3-nagbar -m "Display Chooser" -t warning \
-	-b "Laptop Only"  "xrandr --output DP-2 --off --output LVDS-2 --auto" \
-	-b "Laptop + DP"  "xrandr --output LVDS-2 --auto --output DP-2 --auto --right-of LVDS-2" \
-	-b "DP ONLY"    "xrandr --output LVDS-2 --off --output DP-2 --auto"
+	-b "Laptop Only" "xrandr --output HDMI-1 --off --output eDP-2 --auto" \
+	-b "Laptop + HDMI" "xrandr --output eDP-2 --auto --output HDMI-1 --auto --right-of eDP-2" \
+	-b "HDMI ONLY" "xrandr --output eDP-2 --off --output HDMI-1 --auto"
 
 feh --bg-fill ~/.background.png
